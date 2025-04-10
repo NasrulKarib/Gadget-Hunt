a1e8df48340543adc867d978c9291c55acd4208e
NasrulKarib <karibarian12@gmail.com>
2025-04-10 20:41:22 +0600

    feat(auth): handle Google login in API and store user in database

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import SignupView,LoginView, UserProfileView, AdminDashboardView, ValidateToken
+from .views import SignupView,LoginView, UserProfileView, AdminDashboardView, ValidateToken, FirebaseLoginView, FirebaseSignupView
 
 urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
@@ -7,5 +7,6 @@ urlpatterns = [
     path('profile/', UserProfileView.as_view(), name='user-profile'),
     path('admin-dashboard/', AdminDashboardView.as_view(), name='admin-dashboard'),
     path('validate-token/', ValidateToken.as_view(), name='validate-token'),
-    
+    path('firebase-login/', FirebaseLoginView.as_view(), name='firebase-login'),
+    path('firebase-signup/', FirebaseLoginView.as_view(), name='firebase-signup'),
 ]
