a3eac3566a37859764391bb11695a337ebd9c850
NasrulKarib <karibarian12@gmail.com>
2025-04-15 14:40:29 +0600

    import setUser to route

--- frontend/src/route/Route.jsx
+++ frontend/src/route/Route.jsx
@@ -1,6 +1,7 @@
 import React,{useEffect} from 'react';
 import { BrowserRouter as Router, Routes, Route, Outlet,Navigate} from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import {setUser} from '../redux/slices/authSlices'
 import Header from '../components/header/Header';
 import Footer from '../components/footer/Footer';
 import Login from '../components/auth/Login';
