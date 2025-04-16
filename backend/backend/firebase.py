a7808770eadb3526d3e5abf70c851c3d22cbe16b
NasrulKarib <karibarian12@gmail.com>
2025-04-16 10:52:51 +0600

    fix(firebase): update firebase config

--- backend/backend/firebase.py
+++ backend/backend/firebase.py
@@ -1,5 +1,5 @@
 import firebase_admin
 from firebase_admin import credentials
 
-cred = credentials.Certificate('backend/credentials/gadget-hunt-8f2b6-firebase-adminsdk-fbsvc-2535d979f1.json')
+cred = credentials.Certificate('backend/credentials/gadget-hunt-8f2b6-firebase-adminsdk-fbsvc-956dad0b7c.json')
 firebase_admin.initialize_app(cred)
