28b582434a81bf6fa66ae90bfb2bc0f890d4b840
NasrulKarib <karibarian12@gmail.com>
2025-04-16 12:14:49 +0600

    feat(user): added new attribute image in user model

--- backend/users/models.py
+++ backend/users/models.py
@@ -13,6 +13,7 @@ class Users(models.Model):
     address = models.TextField(blank=True, null=True)
     phone = models.CharField(max_length=20, blank=True, null=True)
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='Customer')
+    image = models.URLField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
